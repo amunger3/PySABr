02c538711a52338c743bf7049f738a3937aa8bef
Alex Munger <munger.alex@gmail.com>
2020-12-08 16:56:39 -0500

    cont. port

--- resources/markov_port.py
+++ resources/markov_port.py
@@ -13,11 +13,28 @@ _bline_def = Counter(
     }
 )
 
+# Format: {event: {state: {outs: [0, 1, 2]}}
+_xtrabase_def = {
+    '1B': {
+        '1B': [0.25, 0.25, 0.25],
+        '2B': [0.30, 0.50, 0.75]
+    },
+    '2B': {
+        '1B': [0.15, 0.25, 0.50]
+    },
+    'OUT': {
+        '1B': [0.05, 0.05, 0.00],
+        '2B': [0.25, 0.25, 0.00],
+        '3B': [0.50, 0.50, 0.00]
+    }
+}
+
 # Calculated Run Expectancies given a batting line
 class RunExpCalc:
 
     def __init__(self):
         self.bl = _bline_def
+        self.xb = _xtrabase_def
         self._outs_9 = 27.0
 
     def calc_frqs(self, bline: Counter = _bline_def):
@@ -36,9 +53,9 @@ class RunExpCalc:
         fq['PA'] = (fq['OBE'] / fq['OUT']) * self._outs_9 + self._outs_9  # Clever
         # Calc rate stats
         rt = {
-            'rtBBO': 1 - fq['SO'] / fq['OUT'],
-            'rtSLG': sum(list(map(lambda x, y: x * y, [1, 2, 3, 4], [fq['1B'], fq['2B'], fq['3B'], fq['HR']]))) / (1 - fq['BB']),
-            'rtAVG': sum([fq['1B'], fq['2B'], fq['3B'], fq['HR']]) / (1 - fq['BB'])
+            'BBO': 1 - fq['SO'] / fq['OUT'],
+            'SLG': sum(list(map(lambda x, y: x * y, [1, 2, 3, 4], [fq['1B'], fq['2B'], fq['3B'], fq['HR']]))) / (1 - fq['BB']),
+            'AVG': sum([fq['1B'], fq['2B'], fq['3B'], fq['HR']]) / (1 - fq['BB'])
         }
         return {'fqs': fq, 'rts': rt}
 
@@ -46,23 +63,44 @@ class RunExpCalc:
 
         # Chance of not scoring from each BaseOut state
         fqrts = self.calc_frqs()
+        xb = self.xb
 
         # Construct XScoreProb Matrix as a dictionary
         ld_bsrn = ['3B', '2B', '1B']
-        n_add_bsrn = {'3B': [2, 1, 0], '2B': [1, 0], '1B': [0]}
         n_outs = [2, 1, 0]
+        n_add_bsrn = {'3B': [2, 1, 0], '2B': [1, 0], '1B': [0]}
         bsox = dict()
         for lead in ld_bsrn:
             bsox[lead] = dict()
-            for add_bsrn in n_add_bsrn[lead]:
-                bsox[lead][add_bsrn] = dict()
-                for out in n_outs:
-                    bsox[lead][add_bsrn][out] = None
+            for out in n_outs:
+                bsox[lead][out] = dict()
+                for add_bsrn in n_add_bsrn[lead]:
+                    bsox[lead][out][add_bsrn] = None
 
-        # Calc from freqs
-        bsox['3B'][2][2] = fqrts['fqs']['OUT']
-        bsox['3B'][2][1] = bsox['3B'][2][2] * fqrts['fqs']['BB'] + fqrts['fqs']['OUT']
-        bsox['3B'][2][0] = bsox['3B'][2][1] * fqrts['fqs']['BB'] + fqrts['fqs']['OUT']
+        # Markov Chaining -> bsox[lead][outs][add_bsrn]
+        # Short defs
+        fq1B = fqrts['fqs']['1B']
+        fqBB = fqrts['fqs']['BB']
+        fqOUT = fqrts['fqs']['OUT']
+        rtBBO = fqrts['rts']['BBO']
+
+        # 3B - 2 outs
+        bsox['3B'][2][2] = fqOUT
+        bsox['3B'][2][1] = bsox['3B'][2][2] * fqBB + fqOUT
+        bsox['3B'][2][0] = bsox['3B'][2][1] * fqBB + fqrts['fqs']['OUT']
+        # 3B - 1 out
+        bsox['3B'][1][2] = bsox['3B'][2][2] * fqOUT * (1 - rtBBO * xb['OUT']['3B'][1])
+        bsox['3B'][1][1] = bsox['3B'][1][2] * fqBB + bsox['3B'][2][1] * fqOUT * (1 - rtBBO * xb['OUT']['3B'][1])
+        bsox['3B'][1][0] = bsox['3B'][1][1] * fqBB + bsox['3B'][2][0] * fqOUT * (1 - rtBBO * xb['OUT']['3B'][1])
+        # 3B - 0 outs
+        bsox['3B'][0][2] = bsox['3B'][1][2] * fqOUT * (1 - rtBBO * xb['OUT']['3B'][0])
+        bsox['3B'][0][1] = bsox['3B'][0][2] * fqBB + bsox['3B'][1][1] * fqOUT * (1 - rtBBO * xb['OUT']['3B'][0])
+        bsox['3B'][0][0] = bsox['3B'][0][1] * fqBB + bsox['3B'][1][0] * fqOUT * (1 - rtBBO * xb['OUT']['3B'][0])
+        # 2B - 2 outs
+        bsox['2B'][2][1] = bsox['3B'][2][2] * (fqBB + fq1B * (1 - xb['1B']['2B'][2])) + fqOUT
+        bsox['2B'][2][0] = bsox['2B'][2][1] * fqBB + bsox['3B'][2][1] * fq1B * (1 - xb['1B']['2B'][2]) + fqOUT
+        # 2B - 1 out
+        bsox['2B'][1][1] = bsox['3B'][1][2] * (fqBB + fq1B * (1 - xb['1B']['2B'][1])) + bsox['3B'][2][1] * fqOUT * rtBBO * xb['OUT']['2B'][1] + bsox['2B'][2][1] * fqOUT * (1 - rtBBO * xb['OUT']['2B'][1])
 
         return bsox
 
